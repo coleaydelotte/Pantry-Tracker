4357c888182846b80c95d405bbda695f21a888a6
Cole Aydelottte <cole.aydelotte@gmail.com>
2024-07-30 05:49:09 -0400

    Cleaned up portion of code.
    Cleaned up portion of code.

--- app/page.js
+++ app/page.js
@@ -33,22 +33,10 @@ export default function Home() {
       <Stack width="800px" height="300px" spacing={2} overflow={"scroll"}>
         {
           items.map((i) => (
-            <Box 
-            key={i}
-            width="100%"
-            height="100px"
-            display={'flex'}
-            justifyContent={'center'}
-            alignItems={'center'}
-            bgcolor={"#f0f0f0"}
-            >
-            <Typography
-            variant="h3"
-            color={"#333"}
-            textAlign={"center"}
-            // fontWeight={"bold"}
-            >
-              { //Capitalizing the first letter of the string
+            <Box key={i} width="100%" height="100px" display={'flex'}
+            justifyContent={'center'} alignItems={'center'} bgcolor={"#f0f0f0"}>
+            <Typography variant="h3" color={"#333"} textAlign={"center"}>
+              {
                 i.charAt(0).toUpperCase() + i.slice(1)
               }
             </Typography>
